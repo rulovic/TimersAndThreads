99dca4f82424e77846a920bd7094f5cb9b9a96a1
Raul Sanchez Fernandez <raul.sanchezfernandez@altran.es>
2018-11-14 10:24:08 +0100

    First commit

---
+++ ThreadTimer/ThreadTimer/mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+
+#include <iostream>
+
+Worker::Worker() : t(this)
+{
+    connect(&t, &QTimer::timeout, this, &Worker::process);
+}
+
+Worker::~Worker()
+{
+    t.stop();
+}
+
+void Worker::process()
+{
+    std::cout << "triggering timer" << std::endl;
+}
+
+void Worker::startWorker()
+{
+    t.start(1000);
+}
+
+/*void Worker::stopProcess()
+{
+   QMetaObject::invokeMethod(&t, "stop", Qt::QueuedConnection);
+}*/
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+{
+    wt.moveToThread(&workerThread);
+    connect(&workerThread, &QThread::started, &wt, &Worker::startWorker);
+    connect(&workerThread, &QThread::finished, &workerThread, &QObject::deleteLater);
+    workerThread.start();
+}
+
+MainWindow::~MainWindow()
+{
+    //wt.stopProcess();
+    workerThread.quit();
+    workerThread.wait();
+}
